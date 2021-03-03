0050dc7f40394edf99c429321dd75b9b7ceac0b9
satoshi-takamatsu <70362048+satoshi-takamatsu@users.noreply.github.com>
2021-03-03 20:53:26 +0900

    FactoryBotの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'namaが空では登録できない' do
+    end
+    it 'emailが空では登録できない' do
+    end
+    it 'passwordが空では登録できない' do
+    end
+  end
 end
