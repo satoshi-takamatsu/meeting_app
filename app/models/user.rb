136c4d592938bc04ff43b75d45830bbdf77086ee
satoshi-takamatsu <70362048+satoshi-takamatsu@users.noreply.github.com>
2021-03-04 20:05:39 +0900

    userテーブルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
         #  :confirmable, :lockable, :timeoutable, :trackable
 
-  validates :name, presence: true
+  
+  with_options presence: true do
+    validates :name
+    validates :email, uniqueness: { case_sensitive: false },
+                      format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :encrypted_password, length: { minimum: 6 }
+  end
+
+  validate :password_complexity
+  validate :email_complexity
+
+  def password_complexity
+    return if password.blank? || password =~ /(?=.*?[a-z])(?=.*?[0-9])/
+    errors.add :password, 'Complexity requirement not met. Please use: half-width characters'
+  end
+
+  def email_complexity
+    return if email.blank? || email =~ /(?=.*?[@])/
+    errors.add :email, 'Complexity requirement not met. Please use: @ characters'
+  end
+
 end
