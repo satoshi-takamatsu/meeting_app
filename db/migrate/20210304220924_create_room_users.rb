73868fac27c1ecabb172eeb28f001ca2ea58e5ff
satoshi-takamatsu <70362048+satoshi-takamatsu@users.noreply.github.com>
2021-03-12 21:28:03 +0900

    ランダム文字列の生成

--- db/migrate/20210304220924_create_room_users.rb
+++ db/migrate/20210304220924_create_room_users.rb
@@ -1,8 +1,10 @@
 class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :room_users do |t|
-      t.references :user,      foreign_key: true
-      t.references :room,      foreign_key: true
+      # t.references :user,      foreign_key: true
+      # t.references :room,      foreign_key: true
+      t.integer :user_id,        foreign_key: true
+      t.integer :room_id,        foreign_key: true
       t.timestamps
     end
   end
